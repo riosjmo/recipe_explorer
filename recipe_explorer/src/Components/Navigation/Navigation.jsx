957067704574512c51d0ddb027df507d6928dec6
rio <riojordan127@yahoo.com>
2025-10-04 17:05:42 -0500

    add responsive design for main page

--- recipe_explorer/src/Components/Navigation/Navigation.jsx
+++ recipe_explorer/src/Components/Navigation/Navigation.jsx
@@ -5,8 +5,8 @@ function Navigation() {
     return (
         <nav className="navigation">
             <ul>
-                <li><Link to="/">Log out</Link></li>
-                <li><Link to="/about">About</Link></li>
+                <li className="navigation__log_out"><Link to="/">Log out</Link></li>
+                <li className="navigation__about"><Link to="/about">About</Link></li>
             </ul>
         </nav>
     );
