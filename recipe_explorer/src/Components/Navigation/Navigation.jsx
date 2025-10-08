9f9cf60da63b429a0115d838194e5c7de38e001a
rio <riojordan127@yahoo.com>
2025-10-08 12:51:16 -0500

    fix bem class names

--- recipe_explorer/src/Components/Navigation/Navigation.jsx
+++ recipe_explorer/src/Components/Navigation/Navigation.jsx
@@ -5,7 +5,7 @@ function Navigation() {
     return (
         <nav className="navigation">
             <ul>
-                <li className="navigation__log_out"><Link to="/">Log out</Link></li>
+                <li className="navigation__log-out"><Link to="/">Log out</Link></li>
                 <li className="navigation__about"><Link to="/about">About</Link></li>
             </ul>
         </nav>
